002c54fd247a2a8b91c5a8b09d599edbad46bb20
Diegogal04 <68531355+Diegogal04@users.noreply.github.com>
2021-09-15 10:00:20 -0500

    checout steps done

---
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Paper, Stepper, Step, StepLabe, Typography, CircularProgress, Divider, Button, StepLabel } from '@material-ui/core'
+import { SettingsPhoneSharp } from '@material-ui/icons';
+
+import useStyles from './styles';
+
+const steps = ['Shipping address', 'Payment details']
+
+const Checkout = () => {
+    const [activeStep, setActiveStep] = useState(0);
+    const classes = useStyles();
+
+    const Form = () => activeStep === 0 ? <AddressForm /> : <PaymentForm />;
+
+    return (
+        <>
+         <div className={classes.toolbar}/>
+         <main className={classes.layout}>
+             <Paper className={classes.paper}>
+                <Typography variant="h4" align="center">Checkout</Typography>
+                <Stepper activeStep={activeStep} className={classes.stepper}>
+                    {steps.map((step) => (
+                        <Step key={step}>
+                            <StepLabel>{step}</StepLabel>
+                        </Step>
+                    ))}
+                </Stepper>
+                {activeStep === steps.length ? <Confirmation /> : <From />}
+             </Paper>
+         </main>
+        </>
+    )
+}
+
+export default Checkout
